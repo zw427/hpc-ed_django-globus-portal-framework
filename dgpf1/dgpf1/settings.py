f60978b8802af6934480f4ae7276f810aecf22a8
John-Paul Navarro <navarro@anl.gov>
2024-02-13 11:10:53 -0600

    Fix STATIC_ROOT configuration

--- dgpf1/dgpf1/settings.py
+++ dgpf1/dgpf1/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/soft/globus_search-1.0/www/static'
+STATIC_ROOT = CONF['STATIC_ROOT']
 STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 
 # Default primary key field type

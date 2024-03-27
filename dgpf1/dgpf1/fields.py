eb6351967c484698f38e35383f906638dd47bdf0
John-Paul Navarro <navarro@anl.gov>
2024-03-27 07:59:16 -0500

    See CHANGELOG v0.6.0

--- dgpf1/dgpf1/fields.py
+++ dgpf1/dgpf1/fields.py
@@ -17,7 +17,7 @@ def detail_result_display_fields(result):
     """
     possible_date_fields = ["Version_Date", "Start_Datetime"]
     info = general_info(result)
-    known_fields = [
+    leading_fields = [
         {"field_name": "Title"},
         {"field_name": "Abstract"},
         {"field_name": "Authors"},
@@ -27,22 +27,23 @@ def detail_result_display_fields(result):
         {"field_name": "Target_Group"},
         {"field_name": "Keywords"},
     ]
-    known_field_names = [fl["field_name"] for fl in known_fields]
-    other_fields = [{"field_name": f} for f in info
-                    if f not in known_field_names]
-    display_fields = known_fields + other_fields + [
+    trailing_fields = [
         {"field_name": "Provider_ID", "display_name": "Provider ID"},
         {"field_name": "Rating"},
     ]
+    known_field_names = [fl["field_name"] for fl in leading_fields + trailing_fields]
+    other_fields = [{"field_name": f} for f in info
+                    if f not in known_field_names]
+    display_fields = leading_fields + other_fields + trailing_fields
     # Populate values from info
     for item in display_fields:
         item["display_name"] = item.get("display_name", item["field_name"].replace("_", " "))
         item["value"] = info.get(item["field_name"])
-        if isinstance(item["value"], list):
-            item["value"] = ", ".join(item["value"])
+#        if isinstance(item["value"], list):
+#            item["value"] = ", ".join(item["value"])
         if item["field_name"] in possible_date_fields:
             try:
-                item["value"] = datetime.datetime.fromisoformat(item["value"].replace("Z", ""))
+                item["value"] = datetime.datetime.fromisoformat(item["value"].replace("Z", "+00:00")).strftime("%b %d  %Y %H:%M:%S %Z%z")
             except Exception:
                 # If the date cannot be parsed, just leave it.
                 pass

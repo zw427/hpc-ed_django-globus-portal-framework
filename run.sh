eb8d5fc47f5bfdcdde51774ab8e7f07c6db04525
Zilu <zw427@cornell.edu>
2024-07-30 11:50:03 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
+conda activate hpced
 export APP_CONFIG='dgpf1/local_settings.py'
 python dgpf1/manage.py migrate
 python dgpf1/manage.py loaddata initial_providers
+open -a Google\ Chrome http://127.0.0.1:8000/
 python dgpf1/manage.py runserver
